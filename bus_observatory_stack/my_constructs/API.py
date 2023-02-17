6dfd583363b750883ea849ddfc83d812fc6c91f8
Anthony Townsend <amt3@nyu.edu>
2023-02-17 15:50:33 -0500

    update deprecated certificate construct

--- bus_observatory_stack/my_constructs/API.py
+++ bus_observatory_stack/my_constructs/API.py
@@ -1,9 +1,10 @@
 from constructs import Construct
 
 from aws_cdk import (
-    Duration,
-    aws_apigateway as apigateway,
     CfnOutput,
+    Duration,
+    aws_iam as iam,
+    aws_apigateway as apigateway,
     aws_lambda_python_alpha as lambda_alpha_,
     aws_lambda as _lambda,
     aws_certificatemanager as acm,
@@ -33,28 +34,68 @@ class BusObservatoryAPI(Construct):
                 "bucket": bucket.bucket_name
                 },
             )
+        
+        # Grant the Lambda function permission to read SSM parameters
+        my_handler.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=["ssm:DescribeParameters"],
+            resources=["*"]
+        ))
+
+        # Grant the Lambda function permission to run an Athena query
+        my_handler.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=["ssm:DescribeParameters"],
+            resources=["*"]
+        ))
+
+        # GRANT THE LAMBDA FUNCTION PERMISSION TO RUN ATHENA QUERIES
+
+        my_handler.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=["athena:StartQueryExecution"],
+            resources=["*"]
+        ))
 
         ################################################################################
         # REST API, Custom Domain
         # following https://cloudbytes.dev/aws-academy/cdk-api-gateway-with-custom-domain
         ################################################################################
 
+        #FIXME: this should be stored in a parameter store or passed down from the parent stack
+        root_domain = "busobservatory.org"
+        subdomain = "beta"
+        fully_qualified_domain_name = subdomain+"."+root_domain
 
         # get the hosted zone
         my_hosted_zone = route53.HostedZone.from_lookup(
             self,
             "BusObservatoryAPI_HostedZone",
-            domain_name="busobservatory.org" #FIXME: this should be stored in a parameter store
+            domain_name=root_domain 
             )
 
-        # create certificate
-        my_certificate = acm.DnsValidatedCertificate(
+        #FIXME: dnsvaidatedcertificate is deprecated
+        # [WARNING] aws-cdk-lib.aws_certificatemanager.DnsValidatedCertificate is deprecated.
+        #   use {@link Certificate} instead
+        # # create certificate
+        # my_certificate = acm.DnsValidatedCertificate(
+        #     self,
+        #     "BusObservatoryAPI_Certificate",
+        #     domain_name="beta.busobservatory.org",
+        #     hosted_zone=my_hosted_zone,
+        #     region="us-east-1"
+        #     )
+
+        # CREATE AN ACM CERTIFICATE
+        my_certificate = acm.Certificate(
             self,
-            "BusObservatoryAPI_Certificate",
-            domain_name="beta.busobservatory.org",
-            hosted_zone=my_hosted_zone,
-            region="us-east-1"
-            )
+            "BusObservatoryStack_Certificate",
+            domain_name=fully_qualified_domain_name,
+            validation=acm.CertificateValidation.from_dns(hosted_zone=my_hosted_zone)
+        )
+
+        # EXPORT THE ARN OF THE CERTIFICATE
+        self.certificate_arn = my_certificate.certificate_arn
 
         # create REST API
         my_api = apigateway.LambdaRestApi(
@@ -73,7 +114,7 @@ class BusObservatoryAPI(Construct):
         route53.ARecord(
             self,
             "BusObservatoryAPI_ApiRecord",
-            record_name="beta",
+            record_name=subdomain,
             zone=my_hosted_zone,
             target=route53.RecordTarget.from_alias(targets.ApiGateway(my_api)),
         )
