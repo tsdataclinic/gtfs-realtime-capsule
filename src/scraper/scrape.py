fe5bb1d7e0f41d922d5db2e6e22d448d3e4cd203
tim <tim.lam@twosigma.com>
2024-08-25 17:14:29 +0100

    Create config loading and actual sleep and scrape logic in scrape.py

--- src/scraper/scrape.py
+++ src/scraper/scrape.py
@@ -1,9 +1,82 @@
+import logging
+import os
+import time
+from typing import Dict
+
+import boto3
+import click
 from google.transit import gtfs_realtime_pb2
 import requests
 
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace')
-feed.ParseFromString(response.content)
-for entity in feed.entity:
-  if entity.HasField('trip_update'):
-    print(entity.trip_update)
+import json
+import structlog
+
+structlog.configure(
+        processors=[
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso", key="ts"),
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+    )
+LOG = structlog.get_logger()
+
+
+def check_config(config: Dict):
+    assert config["s3_bucket"]["uri"]
+    assert config["s3_bucket"]["public_key"]
+    assert config["s3_bucket"]["secret_key"]
+
+
+def load_config(path: str = '../../config/config.json'):
+    with open(path, 'r') as f:
+        config = json.load(f)
+        check_config(config)
+        return config
+
+
+def create_s3_client(s3_config: Dict):
+    session = boto3.Session(
+        aws_access_key_id=s3_config["public_key"],
+        aws_secret_access_key=s3_config["secret_key"],
+    )
+    s3 = session.resource("s3").Bucket(s3_config["uri"])
+    return s3
+
+
+def scrape(url: str):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.content
+    except Exception as err:
+        LOG.error(f"Failed to get response: {err}", url=url)
+
+
+@click.command()
+@click.option("-f", required=True, type=str, help="feed ID to be scraped")
+@click.option("-c", type=str, default="'../../config/config.json'", help="config.json path")
+def main(feed_id, config_path):
+    config = load_config(config_path)
+    s3 = create_s3_client(config["s3_bucket"])
+    url = ""
+    for feed in config["feeds"]:
+        if feed["id"] == feed_id:
+            url = feed["source_info"]["producer_url"]
+    if not url:
+        raise ValueError(f"{feed_id} not found in {config_path}")
+    LOG.info("Start scraping", s3_bucket=s3, url=url)
+
+    while True:
+        content = scrape(url)
+        now = time.time()
+        file_path = f'{config["feeds"][0]["feed_name"]}/{now}.bin'
+        f = open(f"../../data/{file_path}", 'wb')
+        f.write(content)
+        s3.upload_file('file_path', '')
+        LOG.info(f"Scraped at {now}")
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    main()
