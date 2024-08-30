9538efc9718eb95068976892972787c77f284272
george li <george.li98@gmail.com>
2024-08-29 21:19:35 -0400

    working demos / tests

--- gtfs_realtime/read_demo.py
+++ gtfs_realtime/read_demo.py
@@ -1,15 +1,15 @@
-from parquet_utils import read_data
+from gtfs_realtime.parquet_utils import read_data
 import datetime as dt
 import pandas as pd
 
 begin = dt.datetime(2024, 8, 29)
 end = dt.datetime(2024, 8, 30)
 
-uri = "s3://dataclinic-gtfs-rt/gtfs_norm/test/"
+# uri = "s3://dataclinic-gtfs-rt/gtfs_norm/test/mta-subway-ace/trip-updates"
+# uri = "s3://dataclinic-gtfs-rt/gtfs_norm/test/mta-subway-ace/vehicles"
+uri = "s3://dataclinic-gtfs-rt/gtfs_norm/test/mta-subway-alerts/"
 
 table = read_data(uri, begin, end)
 df = table.to_pandas()
 print(df)
 print(df.columns)
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(df[[col for col in df if "trip." in col]])
--- gtfs_realtime/write_demo.py
+++ gtfs_realtime/write_demo.py
@@ -3,42 +3,74 @@ import requests
 from protobuf_utils import protobuf_objects_to_pyarrow_table
 from parquet_utils import write_data, add_time_columns
 import datetime as dt
+from time import sleep
 
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace')
-feed.ParseFromString(response.content)
-
-trip_updates = []
-alerts = []
-vehicles = []
 
 S3_BUCKET = "dataclinic-gtfs-rt"
 
-cur_time = dt.datetime.now()
-cur_date = cur_time.date()
-for entity in feed.entity:
-    entity_id = entity.id
-    if entity.HasField('trip_update'):
-        trip_updates.append((entity_id, entity.trip_update))
-    if entity.HasField('alert'):
-        alerts.append((entity_id, entity.alert))
-    if entity.HasField('vehicle'):
-        vehicles.append((entity_id, entity.vehicle))
+while True:
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace')
+    feed.ParseFromString(response.content)
 
+    trip_updates = []
+    vehicles = []
 
-trip_updates_pa = protobuf_objects_to_pyarrow_table([x[1] for x in trip_updates]) if trip_updates else None
-alerts_pa = protobuf_objects_to_pyarrow_table([x[1] for x in alerts]) if alerts else None
-vehicles_pa = protobuf_objects_to_pyarrow_table([x[1] for x in vehicles]) if alerts else None
+    cur_time = dt.datetime.now()
+    cur_date = cur_time.date()
+    for entity in feed.entity:
+        entity_id = entity.id
+        if entity.HasField('trip_update'):
+            trip_updates.append((entity_id, entity.trip_update))
+        # if entity.HasField('alert'):
+        #     alerts.append((entity_id, entity.alert))
+        if entity.HasField('vehicle'):
+            vehicles.append((entity_id, entity.vehicle))
 
-if trip_updates_pa:
-    trip_updates_pa = trip_updates_pa.add_column(0, "id", [[x[0] for x in trip_updates]])
-    trip_updates_pa = add_time_columns(trip_updates_pa, cur_time, cur_date)
+    trip_updates_pa = protobuf_objects_to_pyarrow_table([x[1] for x in trip_updates]) if trip_updates else None
+    vehicles_pa = protobuf_objects_to_pyarrow_table([x[1] for x in vehicles]) if vehicles else None
 
-s3_uri = f"s3://{S3_BUCKET}/gtfs_norm/test"
-write_data(trip_updates_pa, s3_uri)
+    if trip_updates_pa:
+        trip_updates_pa = trip_updates_pa.add_column(0, "id", [[x[0] for x in trip_updates]])
+        trip_updates_pa = add_time_columns(trip_updates_pa, cur_time, cur_date)
+        print(f"trip-updates count: {len(trip_updates_pa)}")
+        s3_uri = f"s3://{S3_BUCKET}/gtfs_norm/test/mta-subway-ace/trip-updates"
+        write_data(trip_updates_pa, s3_uri)
 
-# if vehicles_pa:
-#     print(vehicles_pa.add_column(0, "id", [[x[0] for x in vehicles]]))
-#
-# if alerts_pa:
-#     print(alerts_pa.add_column(0, "id", [[x[0] for x in alerts]]))
+    if vehicles_pa:
+        vehicles_pa = vehicles_pa.add_column(0, "id", [[x[0] for x in vehicles]])
+        vehicles_pa = add_time_columns(vehicles_pa, cur_time, cur_date)
+        print(f"vehicles count: {len(vehicles_pa)}")
+
+        s3_uri = f"s3://{S3_BUCKET}/gtfs_norm/test/mta-subway-ace/vehicles"
+        write_data(vehicles_pa, s3_uri)
+
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts')
+    feed.ParseFromString(response.content)
+
+    alerts = []
+
+    cur_time = dt.datetime.now()
+    cur_date = cur_time.date()
+    for entity in feed.entity:
+        entity_id = entity.id
+        if entity.HasField('trip_update'):
+            trip_updates.append((entity_id, entity.trip_update))
+        if entity.HasField('alert'):
+            alerts.append((entity_id, entity.alert))
+        if entity.HasField('vehicle'):
+            vehicles.append((entity_id, entity.vehicle))
+
+    alerts_pa = protobuf_objects_to_pyarrow_table([x[1] for x in alerts]) if alerts else None
+
+    if alerts_pa:
+        alerts_pa = alerts_pa.add_column(0, "id", [[x[0] for x in alerts]])
+        alerts_pa = add_time_columns(alerts_pa, cur_time, cur_date)
+        print(f"alerts count: {len(alerts_pa)}")
+
+        s3_uri = f"s3://{S3_BUCKET}/gtfs_norm/test/mta-subway-alerts"
+        write_data(alerts_pa, s3_uri)
+
+    print(cur_time)
+    sleep(60)
