ac467a2ceac155f79935dc3f6848f984d60f0551
George Li <george.li98@gmail.com>
2024-10-03 06:20:37 -0400

    add config for normalize loop parse from raw files (#20)
    * Revert "Revert "Draft: add CLI for normalize from raw files""
    
    This reverts commit f588550ac7c1d23b8baffc3a4e7d3bcfdc68da00.
    
    * cur chages
    
    * update normalization logic to use provided config.json

---
+++ src/normalize/normalize.py
@@ -0,0 +1,191 @@
+import datetime
+import os.path
+import pyarrow as pa
+import click
+import boto3
+from dateutil import parser
+import datetime as dt
+from gtfs_realtime_pb2 import FeedMessage
+from src.normalize.protobuf_utils import protobuf_objects_to_pyarrow_table
+from src.normalize.parquet_utils import write_data, add_time_columns
+import time
+import json
+import structlog
+import logging
+
+structlog.configure(
+    processors=[
+        structlog.processors.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso", key="ts"),
+        structlog.processors.JSONRenderer(),
+    ],
+    wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+)
+LOGGER = structlog.get_logger()
+SCRIPT_DIR = os.path.dirname(__file__)
+CONFIG_DIR = f"{SCRIPT_DIR}/../../config"
+DATA_DIR = f"{SCRIPT_DIR}/../../data"
+
+
+def check_config(config: dict):
+    assert config["raw_prefix"]
+    assert config["normalize_prefix"]
+
+    assert config["feed_id"]
+
+    assert config["normalize_config"]["start_date"]
+    assert config["normalize_config"]["state_file"]
+
+
+def load_config(path: str):
+    with open(path, "r") as f:
+        config = json.load(f)
+        check_config(config)
+        return config
+
+
+def get_last_processed_timestamp(s3, bucket, key):
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        return dt.datetime.fromtimestamp(float(json.loads(response['Body'].read())['last_processed']))
+    except s3.exceptions.NoSuchKey:
+        return None
+
+
+def update_last_processed_timestamp(s3, bucket, key, timestamp):
+    s3.put_object(
+        Bucket=bucket,
+        Key=key,
+        Body=json.dumps({'last_processed': timestamp})
+    )
+
+
+def normalize_raw_feed(raw_input, cur_time, cur_date):
+    feed = FeedMessage()
+    feed.ParseFromString(raw_input)
+
+    trip_updates = []
+    vehicles = []
+    alerts = []
+
+    for entity in feed.entity:
+        entity_id = entity.id
+        if entity.HasField('trip_update'):
+            trip_updates.append((entity_id, entity.trip_update))
+        if entity.HasField('alert'):
+            alerts.append((entity_id, entity.alert))
+        if entity.HasField('vehicle'):
+            vehicles.append((entity_id, entity.vehicle))
+
+    trip_updates_pa = protobuf_objects_to_pyarrow_table([x[1] for x in trip_updates]) if trip_updates else None
+    vehicles_pa = protobuf_objects_to_pyarrow_table([x[1] for x in vehicles]) if vehicles else None
+    alerts_pa = protobuf_objects_to_pyarrow_table([x[1] for x in alerts]) if alerts else None
+
+    if trip_updates_pa:
+        trip_updates_pa = trip_updates_pa.add_column(0, "id", [[x[0] for x in trip_updates]])
+        trip_updates_pa = add_time_columns(trip_updates_pa, cur_time, cur_date)
+
+    if vehicles_pa:
+        vehicles_pa = vehicles_pa.add_column(0, "id", [[x[0] for x in vehicles]])
+        vehicles_pa = add_time_columns(vehicles_pa, cur_time, cur_date)
+
+    if alerts_pa:
+        alerts_pa = alerts_pa.add_column(0, "id", [[x[0] for x in alerts]])
+        alerts_pa = add_time_columns(alerts_pa, cur_time, cur_date)
+
+    return trip_updates_pa, vehicles_pa, alerts_pa
+
+
+def parse_files(source_prefix, destination_prefix, start_date, state_file):
+    s3 = boto3.client('s3')
+
+    source_bucket, source_key = source_prefix.replace("s3://", "").split("/", 1)
+    state_bucket, state_key = state_file.replace("s3://", "").split("/", 1)
+
+    last_processed = get_last_processed_timestamp(s3, state_bucket, state_key)
+    if last_processed:
+        LOGGER.info(f"Loaded last_processed timestamp of {last_processed}")
+    else:
+        LOGGER.info(f"No state information found at {state_file}, defaulting `last_processed={start_date}")
+        last_processed = parser.parse(start_date)
+
+    # List objects in the source bucket
+    paginator = s3.get_paginator('list_objects_v2')
+
+    cur_date = datetime.datetime.now()
+    cur_processing = last_processed
+    while cur_processing < cur_date:
+        date_partition = os.path.join(source_key, str(cur_processing.year), str(cur_processing.month), str(cur_processing.day))
+        LOGGER.info(f"Processing date: {date_partition}")
+        max_timestamp = last_processed.timestamp()
+
+        trip_updates_pa, vehicles_pa, alerts_pa = None, None, None
+
+        for page in paginator.paginate(Bucket=source_bucket, Prefix=date_partition):
+            for obj in page.get('Contents', []):
+                key = obj['Key']
+                if key.endswith('.binpb'):
+                    # Check if this file is newer than the last processed file
+                    file_write_time = float(key.split('/')[-1].removesuffix('.binpb'))
+                    if file_write_time > last_processed.timestamp():
+                        LOGGER.info(f"Processing file: {key}")
+                        # Download the file
+                        response = s3.get_object(Bucket=source_bucket, Key=key)
+                        file_content = response['Body'].read()
+
+                        cur_trip_updates_pa, cur_vehicles_pa, cur_alerts_pa = normalize_raw_feed(
+                            file_content,
+                            obj['LastModified'],
+                            obj['LastModified'].date()
+                        )
+
+                        trip_updates_pa = pa.concat_tables([trip_updates_pa, cur_trip_updates_pa]) if trip_updates_pa else cur_trip_updates_pa
+                        vehicles_pa = pa.concat_tables([vehicles_pa, cur_vehicles_pa]) if vehicles_pa else cur_vehicles_pa
+                        alerts_pa = pa.concat_tables([alerts_pa, cur_alerts_pa]) if alerts_pa else cur_alerts_pa
+
+                    max_timestamp = max(max_timestamp, file_write_time)
+
+            if trip_updates_pa:
+                s3_uri = f"{destination_prefix}/trip-updates"
+                LOGGER.info(f"Writing {trip_updates_pa.num_rows} entries to {s3_uri}")
+                write_data(trip_updates_pa, s3_uri)
+            if vehicles_pa:
+                s3_uri = f"{destination_prefix}/vehicles"
+                LOGGER.info(f"Writing {vehicles_pa.num_rows} entries to {s3_uri}")
+                write_data(vehicles_pa, s3_uri)
+            if alerts_pa:
+                s3_uri = f"{destination_prefix}/alerts"
+                LOGGER.info(f"Writing {alerts_pa.num_rows} entries to {s3_uri}")
+                write_data(alerts_pa, s3_uri)
+
+            # Update the last processed timestamp
+            if max_timestamp == last_processed:
+                LOGGER.warning(f"No data found in partition: {date_partition} - is this expected?")
+            LOGGER.info(f"Updating last processed timestamp to maximum file timestamp: {max_timestamp}")
+            update_last_processed_timestamp(s3, state_bucket, state_key, max_timestamp)
+        cur_processing += datetime.timedelta(days=1)
+
+
+@click.option(
+    "-c",
+    "--config_path",
+    type=str,
+    default=f"{CONFIG_DIR}/config.json",
+    help="config.json path",
+)
+def main(config_path):
+    config = load_config(config_path)
+    norm_config = config["normalize_config"]
+    feed_id = config["feed_id"]
+    while True:
+        parse_files(
+            source_prefix=os.path.join(config["raw_prefix"], feed_id),
+            destination_prefix=os.path.join(config["normalize_prefix"], feed_id),
+            start_date=norm_config["start_date"],
+            state_file=norm_config["state_file"]
+        )
+        time.sleep(norm_config["loop_interval"])
+
+
+if __name__ == "__main__":
+    main(f"{CONFIG_DIR}/config.json")
